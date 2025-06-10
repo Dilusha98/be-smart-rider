54bd6cdaa7514fcaf4aa8d97770a939237c3029e
dilusha senevirathna <dilusha.xypherlabs@gmail.com>
2025-06-11 01:59:17 +0530

    v

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    // API_BASE_URL: "http://127.0.0.1:8000/api",
-    // API_image_URL: "http://127.0.0.1:8000",
-     API_BASE_URL: "https://ride.redfernmigration.com/api",
-    API_image_URL: "https://ride.redfernmigration.com",
+    API_BASE_URL: "http://127.0.0.1:8000/api",
+    API_image_URL: "http://127.0.0.1:8000",
+    //  API_BASE_URL: "https://ride.redfernmigration.com/api",
+    // API_image_URL: "https://ride.redfernmigration.com",
 };
